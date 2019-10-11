cf4f37f1bcd22b53f0cb372e34f5734b4bbb0983
LockyGT <isnavest@live.com.mx>
2019-10-11 17:54:51 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Curso from './Curso';
+import "./styles/styles.scss"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <>
+  <div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" src="https://cdn.pixabay.com/photo/2019/10/05/09/57/landscape-4527526_960_720.jpg" alt="banner"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Información</p>
+        <p> Hola Mundo</p>
+        <a href="https://ed.team" className="button">Abrir</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+
+  <div className="ed-grid m-grid-3">
+      <Curso />
+      <Curso />
+      <Curso />
+  </div>
+  
+  </>
+
+  
+)
 
 export default App;
+
+// Reglas JXS
+// 1.- Toda etiqueta debe cerrarse 
+// 2.- Los componentes deben devolver un solo elemento padre
+// 3.- Apoyarse de los Fragment Cuando necesito debolver 2 elementos
+// 4.- Fragment => <> hijos </>
+// 5.- img siempre se cierra
+// 6.- Class => className
+// 7.- for => htmlFor
